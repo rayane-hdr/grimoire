3c2d63258c3a634ec0f7bb6120470d6690c65146
rayane <rayaneelhadri1@gmail.com>
2026-03-02 19:55:20 +0100

    feat: create User model

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-// app.js
+require("dotenv").config();
+
+
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ Connected to MongoDB Atlas"))
+  .catch((error) => console.error("❌ MongoDB connection error:", error));
 
 // Middlewares
 app.use(cors());
